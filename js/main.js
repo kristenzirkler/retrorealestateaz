81c0a185d6947976e2db51c3761d8f0421f3c110
Kristen Zirkler <bazirkler@gmail.com>
2015-11-18 15:23:01 -0700

    added image changing & darkened photo

--- js/main.js
+++ js/main.js
@@ -7,4 +7,24 @@ $(function(){
 		$('.nav').toggleClass('nav-open');
 	});
 
+    $('.intro-background').css('background-image', 'url(../img/home-photo-5.jpg').fadeIn(100);
+
+	var homeImages = [];
+	homeImages[0] = "../img/home-photo-0.jpg";
+	homeImages[1] = "../img/home-photo-1.jpg";
+	homeImages[2] = "../img/home-photo-2.jpg";
+	homeImages[3] = "../img/home-photo-3.jpg";
+	homeImages[4] = "../img/home-photo-4.jpg";
+	homeImages[5] = "../img/home-photo-5.jpg";
+
+	var i = 0;
+	setInterval(fadeDivs, 5000);
+
+	function fadeDivs() {
+	    i = i < homeImages.length ? i : 0;
+	    $('.intro-background').fadeOut(500, function(){
+	        $(this).css('background-image', 'url('+homeImages[i]+')').fadeIn(500);
+	    })
+	    i++;
+	}
 });
